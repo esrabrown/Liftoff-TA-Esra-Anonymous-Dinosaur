c94e1d3b079b78059496aa253856c0a1fccf8ddc
JohnProphet <johnldudash@gmail.com>
2024-03-27 23:46:41 -0500

    Started building Stripe Integration.  Unfinished.

--- src/main/java/com/cafemanager/cafe/StripePaymentApplication.java
+++ src/main/java/com/cafemanager/cafe/StripePaymentApplication.java
@@ -1,8 +1,9 @@
 package com.cafemanager.cafe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class StripePaymentApplication {
 
     public static void main(String[] args) {
