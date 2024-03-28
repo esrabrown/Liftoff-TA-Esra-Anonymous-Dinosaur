90d54b8322d9d8b9f603a37d46c9a97dba0310d1
JohnProphet <87950318+JohnProphet@users.noreply.github.com>
2024-03-28 18:37:52 -0500

    Minor change.  Still broken.

--- src/main/java/StripePaymentController/StripePaymentController.java
+++ src/main/java/StripePaymentController/StripePaymentController.java
@@ -1,9 +1,11 @@
 package StripePaymentController;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+// @RestController was suggested on stripe API page
 public class StripePaymentController {
 
     @Value("${stripe.apikey}")
