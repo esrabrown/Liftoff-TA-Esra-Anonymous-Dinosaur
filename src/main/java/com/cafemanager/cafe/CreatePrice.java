9708b12003d1bab20f61e9cc0c65ee21eff7deaa
JohnProphet <johnldudash@gmail.com>
2024-04-15 14:44:32 -0500

    Basic But Almost Working, Maybe?

---
+++ src/main/java/com/cafemanager/cafe/CreatePrice.java
@@ -0,0 +1,38 @@
+package com.cafemanager.cafe;
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Product;
+import com.stripe.param.ProductCreateParams;
+import com.stripe.param.PriceCreateParams;
+import com.stripe.model.Price;
+
+public class CreatePrice {
+    public static void main(String[] args) throws StripeException {
+        Stripe.apiKey = "sk_test_51Oz9N1LnssI0iBqMPWFgTeI9wYxKQmPbtFRKuTmczAHzCR6gFnwjisbXMdkNhoCNHhk6X39ufo0JTTwCuW4OCPDp00eIMwzVu2";
+
+
+        ProductCreateParams productParams =
+                ProductCreateParams.builder()
+                        .setName("Monthly Subscription")
+                        .setDescription("$50/Month subscription")
+                        .build();
+        Product product = Product.create(productParams);
+        System.out.println("Success! Here is your starter subscription product id: " + product.getId());
+
+        PriceCreateParams params =
+                PriceCreateParams
+                        .builder()
+                        .setProduct(product.getId())
+                        .setCurrency("usd")
+                        .setUnitAmount(1200L)
+                        .setRecurring(
+                                PriceCreateParams.Recurring
+                                        .builder()
+                                        .setInterval(PriceCreateParams.Recurring.Interval.MONTH)
+                                        .build())
+                        .build();
+        Price price = Price.create(params);
+        System.out.println("Success! Here is your starter subscription price id: " + price.getId());
+    }
+}
